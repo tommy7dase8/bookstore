881b2988f1c0c59d4173539aa8296e5c7f04dcf8
tommy7dase8 <10211900416@stu.ecnu.edu.cn>
2023-12-22 12:36:44 +0800

    out db_conn

---
+++ be/db_conn.py
@@ -0,0 +1,26 @@
+from init_database import *
+
+
+def user_id_exist(user_id):
+    cursor = session.query(User).filter(User.user_id == user_id).first()
+    if (cursor is None):
+        return False
+    else:
+        return True
+
+
+def book_id_exist(store_id, book_id):
+    cursor = session.query(Store_detail).filter(Store_detail.store_id == store_id,
+                                                Store_detail.book_id == book_id).first()
+    if (cursor is None):
+        return False
+    else:
+        return True
+
+
+def store_id_exist(store_id):
+    cursor = session.query(Store).filter(Store.store_id == store_id).first()
+    if (cursor is None):
+        return False
+    else:
+        return True
