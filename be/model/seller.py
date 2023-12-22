149190378bd53b7f02052a726908823de1089c5f
tommy7dase8 <10211900416@stu.ecnu.edu.cn>
2023-12-22 12:39:27 +0800

    finish seller

--- be/model/seller.py
+++ be/model/seller.py
@@ -1,76 +1,66 @@
-import sqlite3 as sqlite
 from be.model import error
-from be.model import db_conn
+from be.db_conn import *
+import json
+from sqlalchemy import and_
 
+class Seller():
+    def add_book(self, user_id: str, store_id: str, book_id: str, book_json_str: str, stock_level: int):
+        book_json = json.loads(book_json_str)
+        if not user_id_exist(user_id):
+            return error.error_non_exist_user_id(user_id)
+        if not store_id_exist(store_id):
+            return error.error_non_exist_store_id(store_id)
+        if book_id_exist(store_id, book_id):
+            return error.error_exist_book_id(book_id)
+        book_one = Book(book_id=book_id,
+                        title=book_json.get("title"),
+                        author=book_json.get("author"),
+                        publisher=book_json.get("publisher"),
+                        original_title=book_json.get("original_title"),
+                        translator=book_json.get("translator"),
+                        pub_year=book_json.get("pub_year"),
+                        pages=book_json.get("pages"),
+                        original_price=book_json.get("price"),
+                        currency_unit=book_json.get("currency_unit"),
+                        binding=book_json.get("binding"),
+                        isbn=book_json.get("isbn"),
+                        author_intro=book_json.get("author_intro"),
+                        book_intro=book_json.get("book_intro"),
+                        content=book_json.get("content"),
+                        tags=book_json.get("tags"),
+                        picture=book_json.get("picture")
+                        )
 
-class Seller(db_conn.DBConn):
-    def __init__(self):
-        db_conn.DBConn.__init__(self)
-
-    def add_book(
-        self,
-        user_id: str,
-        store_id: str,
-        book_id: str,
-        book_json_str: str,
-        stock_level: int,
-    ):
-        try:
-            if not self.user_id_exist(user_id):
-                return error.error_non_exist_user_id(user_id)
-            if not self.store_id_exist(store_id):
-                return error.error_non_exist_store_id(store_id)
-            if self.book_id_exist(store_id, book_id):
-                return error.error_exist_book_id(book_id)
-
-            self.conn.execute(
-                "INSERT into store(store_id, book_id, book_info, stock_level)"
-                "VALUES (?, ?, ?, ?)",
-                (store_id, book_id, book_json_str, stock_level),
-            )
-            self.conn.commit()
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
-        except BaseException as e:
-            return 530, "{}".format(str(e))
+        store_detail_one = Store_detail(
+            store_id=store_id,
+            book_id=book_id,
+            stock_level=stock_level,
+            price=book_json.get("price")
+        )
+        session.add(store_detail_one)
+        session.commit()
         return 200, "ok"
 
-    def add_stock_level(
-        self, user_id: str, store_id: str, book_id: str, add_stock_level: int
-    ):
-        try:
-            if not self.user_id_exist(user_id):
-                return error.error_non_exist_user_id(user_id)
-            if not self.store_id_exist(store_id):
-                return error.error_non_exist_store_id(store_id)
-            if not self.book_id_exist(store_id, book_id):
-                return error.error_non_exist_book_id(book_id)
+    def add_stock_level(self, user_id: str, store_id: str, book_id: str, add_stock_level: int):
+        if not user_id_exist(user_id):
+            return error.error_non_exist_user_id(user_id)
+        if not store_id_exist(store_id):
+            return error.error_non_exist_store_id(store_id)
+        if not book_id_exist(store_id, book_id):
+            return error.error_non_exist_book_id(book_id)
 
-            self.conn.execute(
-                "UPDATE store SET stock_level = stock_level + ? "
-                "WHERE store_id = ? AND book_id = ?",
-                (add_stock_level, store_id, book_id),
-            )
-            self.conn.commit()
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
-        except BaseException as e:
-            return 530, "{}".format(str(e))
+        cursor = session.query(Store_detail).filter(
+            and_(Store_detail.store_id == store_id, Store_detail.book_id == book_id)).first()
+        cursor.stock_level = cursor.stock_level + add_stock_level
+        session.commit()
         return 200, "ok"
 
     def create_store(self, user_id: str, store_id: str) -> (int, str):
-        try:
-            if not self.user_id_exist(user_id):
-                return error.error_non_exist_user_id(user_id)
-            if self.store_id_exist(store_id):
-                return error.error_exist_store_id(store_id)
-            self.conn.execute(
-                "INSERT into user_store(store_id, user_id)" "VALUES (?, ?)",
-                (store_id, user_id),
-            )
-            self.conn.commit()
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
-        except BaseException as e:
-            return 530, "{}".format(str(e))
+        if not user_id_exist(user_id):
+            return error.error_non_exist_user_id(user_id)
+        if store_id_exist(store_id):
+            return error.error_exist_store_id(store_id)
+        store_one = Store(user_id=user_id, store_id=store_id)
+        session.add(store_one)
+        session.commit()
         return 200, "ok"
